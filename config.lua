4382871200a7e5d7617d6c5bdd7ead1452e8eec7
Rob Champagne <68358594+LtDoc@users.noreply.github.com>
2021-01-03 23:38:29 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,31 @@
+Config = {}
+Config.lang = 'en'
+
+Config.JobName = 'Oiler'
+Config.Vehicle = 'oilWagon01x'
+Config.item = 'rawpetrol'
+
+Config.money = {
+    ['min'] = 25,
+    ['max'] = 35
+}
+
+Config.xp = {
+    ['min'] = 22,
+    ['max'] = 50
+}
+
+Config.itemcount = {
+	['min'] = 1,
+    ['max'] = 5
+}
+
+Config.Zones = {
+    ['init'] = { x = 555.3455, y = 601.9536, z = 109.9179 },
+    ['Mine1'] = { x = 562.4064, y = 599.1511, z = 110.3586 },
+    ['Mine2'] = { x = 594.7307, y = 584.0449, z = 110.3604 },
+    ['Mine3'] = { x = 572.2089, y = 551.5209, z = 110.2125 },
+    ['Mine4'] = { x = 537.0079, y = 618.6827, z = 111.2549 },
+    ['Vehicle'] = { x = 537.0079, y = 618.6827, z = 111.2549, heading = 269.01 },
+    ['Delivery'] = { x = 1520.859, y = 414.2325, z= 89.0776 }
+}
